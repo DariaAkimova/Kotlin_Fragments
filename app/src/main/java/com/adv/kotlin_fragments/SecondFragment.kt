b7c6344140fc3298f47653e295374ca85b1d5cee
Daria Akimova <akimova-dv@mail.ru>
2022-12-08 19:50:25 +0300

    Initial commit

---
+++ app/src/main/java/com/adv/kotlin_fragments/SecondFragment.kt
@@ -0,0 +1,32 @@
+package com.adv.kotlin_fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.adv.kotlin_fragments.databinding.FragmentFirstBinding
+import com.adv.kotlin_fragments.databinding.FragmentSecondBinding
+
+
+class SecondFragment : Fragment() {
+    lateinit var binding: FragmentSecondBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSecondBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.button2.setOnClickListener{
+            Navigation.findNavController(it).navigate(R.id.action_secondFragment_to_firstFragment)
+        }
+    }
+
+
+}
